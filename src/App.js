48911b9de65bf45304650113bf987a9af5f62cb6
Sean Yuan <ysean54@yahoo.com>
2022-06-19 11:33:48 -0400

    Food Recipes

--- src/App.js
+++ src/App.js
@@ -3,52 +3,53 @@ import './App.css';
 import Navbar from './component/Navbar'
 import RecipeList from './component/RecipeList'
 
-const RecipeAPIURL = 'www.themealdb.com/api/json/v1/1/search.php?s='
+const Api = "https://www.themealdb.com/api/json/v1/1/search.php?s=";
 
 function App() {
-
-  const [loading, setLoading] = useState(false)
-  const [select, setSelect] = useState("")
-  const [recipes, setRecipes] = useState([])
-
-  const searchRecipe = async () => {
-    setLoading(true)
-    const Url = RecipeAPIURL + select;
-    const res = await fetch(Url);
+  const [loading, setLoading] = useState(false);
+  const [select, setSelect] = useState("");
+  const [recipes, setRecipes] = useState([]);
+  
+  // search for the recipe
+  const searchRecipes = async () => {
+    setLoading(true);
+    const url = Api + select
+    const res = await fetch(url);
     const data = await res.json();
-    setRecipes(data.meals)
-    setLoading(false)
-
+    setRecipes(data.meals);
+    setLoading(false);
   };
 
   useEffect(() => {
-    searchRecipe();
-  },[])
+    searchRecipes()
+  }, []);
 
-  const handleSubmit = event => {
-    event.preventDefault()
-    searchRecipe()
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    searchRecipes();
   }
 
   return (
     <div className="container">
-      <h1>Recipes</h1>
-      <Navbar 
-        handleSubmit={handleSubmit}
-        value={select}
-        onChange={(event) => setSelect(event.target.value)}
+      <h2 className="title">Our Food Recipes</h2>
+      <Navbar
         loading={loading}
+        value={select}
+        handleSubmit={handleSubmit}
+        onChange={(event) => setSelect(event.target.value)}
       />
       <div className="recipes">
+        
         {recipes ? recipes.map(recipe => (
-          <RecipeList 
-            key={recipe.idMeal}
-            recipe={recipe}
+          <RecipeList
+             key={recipe.idMeal}
+             recipe={recipe}
           />
-        )) : "No Recipe found"}
+        )) : "No Results."}
       </div>
     </div>
   );
 }
 
 export default App;
+
